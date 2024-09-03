e0a911be5a0789f84d5b754319522ee7e90c1977
David Ambrozio <davidambroziodev@gmail.com>
2024-09-03 12:27:30 -0300

    feat: create cronjob

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
+import { CronJob } from 'cron';
+
+import { createPost } from "./blueskyHandler";
 import { download, getURL } from "./imageHandler";
 
 async function main() {
   try {
     const imageURL = await getURL();
-    download(imageURL);
-
+    await download(imageURL);
     
+    createPost()
   } catch (error) {
     console.error('Error in main function:', error);
   }
 }
 
 main();
+
+const scheduleExpression5Minutes = '*/5 * * * *';
+const scheduleExpression = '0 */3 * * *';
+
+const job = new CronJob(scheduleExpression5Minutes, main);
+
+job.start();
